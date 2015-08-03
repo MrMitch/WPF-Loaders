aad41132d8a7c6937ff79d042e19c88f61340ea1
Mickael GOETZ <contact@mickael-goetz.com>
2015-08-03 14:51:49 +0200

    Imgur

---
+++ Loaders/ImgurLoader.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Loaders
+{
+    /// <summary>
+    /// Interaction logic for ImgurLoader.xaml
+    /// </summary>
+    public partial class ImgurLoader : UserControl
+    {
+        public static readonly DependencyProperty RingsThicknessProperty = DependencyProperty.Register(
+            "RingsThickness", 
+            typeof (double), 
+            typeof (ImgurLoader), 
+            new PropertyMetadata(10d)
+        );
+
+        public double RingsThickness
+        {
+            get { return (double) GetValue(RingsThicknessProperty); }
+            set { SetValue(RingsThicknessProperty, value); }
+        }
+
+
+        public static readonly DependencyProperty RingsBrushProperty = DependencyProperty.Register(
+            "RingsBrush", typeof (Brush), typeof (ImgurLoader), new PropertyMetadata(default(Brush)));
+
+        public Brush RingsBrush
+        {
+            get { return (Brush) GetValue(RingsBrushProperty); }
+            set { SetValue(RingsBrushProperty, value); }
+        }
+
+        public ImgurLoader()
+        {
+            InitializeComponent();
+        }
+    }
+}
