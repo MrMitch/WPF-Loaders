404af230515141861c85c61bfbfe6d84a6cca205
Mickael GOETZ <contact@mickael-goetz.com>
2015-08-08 22:05:38 +0200

    allow to override the style of ImgurLoader and ChromeLoader

--- Loaders/ImgurLoader.cs
+++ Loaders/ImgurLoader.cs
@@ -3,45 +3,42 @@ using System.Windows.Controls;
 
 namespace Loaders
 {
-    /// <summary>
-    /// Imgur inspired loader
-    /// </summary>
-    public partial class ImgurLoader : UserControl
+    public class ImgurLoader : Control
     {
         #region IsIndeterminate
         public static readonly DependencyProperty IsIndeterminateProperty = DependencyProperty.Register(
             "IsIndeterminate",
-            typeof (bool),
-            typeof (ImgurLoader),
+            typeof(bool),
+            typeof(ImgurLoader),
             new PropertyMetadata(default(bool))
-            );
+        );
 
         public bool IsIndeterminate
         {
-            get { return (bool) GetValue(IsIndeterminateProperty); }
+            get { return (bool)GetValue(IsIndeterminateProperty); }
             set { SetValue(IsIndeterminateProperty, value); }
         }
         #endregion
-        
+
         #region RingsThickness
         public static readonly DependencyProperty RingsThicknessProperty = DependencyProperty.Register(
             "RingsThickness",
-            typeof (double),
-            typeof (ImgurLoader),
-            new PropertyMetadata(10d)
-            );
+            typeof(double),
+            typeof(ImgurLoader),
+            new PropertyMetadata(default(double))
+        );
 
         public double RingsThickness
         {
-            get { return (double) GetValue(RingsThicknessProperty); }
+            get { return (double)GetValue(RingsThicknessProperty); }
             set { SetValue(RingsThicknessProperty, value); }
         }
         #endregion
 
 
-        public ImgurLoader()
+        static ImgurLoader()
         {
-            InitializeComponent();
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ImgurLoader), new FrameworkPropertyMetadata(typeof(ImgurLoader)));
         }
     }
 }
